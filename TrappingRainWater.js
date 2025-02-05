fa699a2bc29c8f3203f98b20971b461293c8118e
GoodMorning9000 <92028717+GoodMorning9000@users.noreply.github.com>
2025-02-04 21:55:15 -0500

    Create TrappingRainWater.js

---
+++ TrappingRainWater.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let res = 0;
+
+    for (let left = 0, right = 0, tempRes = 0; left < height.length; left++, right = left) {
+        tempRes = 0;
+
+        if (height[left] >= height[left + 1]) {
+            while (right < height.length && height[right] < height[left]) {
+                right++;
+                tempRes += (height[left] - height[right]);
+            }
+            if (right = height.length) {
+                continue;
+            }
+            else {
+                res += tempRes;
+                left = right;
+            }
+        }
+    }
+
+    return res;
+};
