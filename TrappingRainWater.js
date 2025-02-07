ec97f9a0fba5d1274c1f74c78c55d33af94b8007
GoodMorning9000 <92028717+GoodMorning9000@users.noreply.github.com>
2025-02-07 02:06:00 +0000

    sigma sigma sigma

--- TrappingRainWater.js
+++ TrappingRainWater.js
@@ -3,27 +3,25 @@
  * @return {number}
  */
 var trap = function(height) {
-    let res = 0;
-
-    for (let left = 0, right = 0, tempRes = 0; left < height.length; left++, right = left) {
-        tempRes = 0;
-
-        if (height[left] >= height[left + 1]) {
-            while (right < height.length && height[right] < height[left]) {
-                right++;
-                tempRes += (height[left] - height[right]);
-            }
-            if (right = height.length) {
-                continue;
-            }
-            else {
-                res += tempRes;
-                left = right;
-            }
+    let leftMax = 0,
+        rightMax = 0,
+        left = 0,
+        right = height.length - 1,
+        water = 0;
+    
+    while (left < right) {
+        if (leftMax < rightMax) {
+            left++;
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+        }
+        else {
+            right--;
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
         }
     }
+    return water;
+}
 
-    return res;
-};
-
-trap([0,1,0,2,1,0,1,3,2,1,2,1]);
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]));
